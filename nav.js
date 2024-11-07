dadca3b01e5e85160831d7200372e3a323271e6d
Dante <dantrux22@gmail.com>
2024-11-07 00:10:00 -0300

    .

--- nav.js
+++ nav.js
@@ -1,15 +1,43 @@
+
 document.addEventListener('DOMContentLoaded', function() {
     const menuToggle = document.getElementById('menu-toggle');
     const navMenu = document.getElementById('nav-menu');
     const navLinks = document.querySelectorAll('.nave-1');
-
+  
     menuToggle.addEventListener('click', function() {
-        navMenu.classList.toggle('show');
+      navMenu.classList.toggle('show');
+      menuToggle.querySelector('span:nth-child(1)').classList.toggle('hide');
+      menuToggle.querySelector('span:nth-child(2)').classList.toggle('show');
     });
-
+  
     navLinks.forEach(function(link) {
-        link.addEventListener('click', function() {
-            navMenu.classList.remove('show');
-        });
+      link.addEventListener('click', function() {
+        navMenu.classList.remove('show');
+        menuToggle.querySelector('span:nth-child(1)').classList.remove('hide');
+        menuToggle.querySelector('span:nth-child(2)').classList.remove('show');
+      });
     });
+  });
+  
+
+// Galeria
+
+  const flechaIzquierda = document.getElementById('flecha-izquierda');
+const flechaDerecha = document.getElementById('flecha-derecha');
+const fotos = document.querySelector('.fotos');
+
+flechaIzquierda.addEventListener('click', () => {
+  fotos.scrollBy({
+    top: 0,
+    left: -300,
+    behavior: 'smooth'
+  });
+});
+
+flechaDerecha.addEventListener('click', () => {
+  fotos.scrollBy({
+    top: 0,
+    left: 300,
+    behavior: 'smooth'
+  });
 });
