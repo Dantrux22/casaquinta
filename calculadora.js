e7a1f64a52ae74432dd4ee28c0b90287bdf376e2
Dante <dantrux22@gmail.com>
2024-12-09 23:27:49 -0300

    .

--- calculadora.js
+++ calculadora.js
@@ -106,7 +106,7 @@ const months = [
           { day: "19", status: "available" }, { day: "20", status: "available" }, { day: "21", status: "reserved" },
           { day: "22", status: "reserved" }, { day: "23", status: "reserved" }, { day: "24", status: "reserved" },
           { day: "25", status: "reserved" }, { day: "26", status: "available" }, { day: "27", status: "available" },
-          { day: "28", status: "available" }, { day: "29", status: "available" }, { day: "30", status: "available" },
+          { day: "28", status: "available" }, { day: "29", status: "reserved" }, { day: "30", status: "available" },
           { day: "31", status: "reserved" }
         ],
         night: [
@@ -133,7 +133,7 @@ const months = [
           { day: "7", status: "available" }, { day: "8", status: "available" }, { day: "9", status: "available" },
           { day: "10", status: "available" }, { day: "11", status: "reserved" }, { day: "12", status: "available" },
           { day: "13", status: "available" }, { day: "14", status: "available" }, { day: "15", status: "available" },
-          { day: "16", status: "available" }, { day: "17", status: "available" }, { day: "18", status: "available" },
+          { day: "16", status: "available" }, { day: "17", status: "available" }, { day: "18", status: "reserved" },
           { day: "19", status: "reserved" }, { day: "20", status: "available" }, { day: "21", status: "available" },
           { day: "22", status: "available" }, { day: "23", status: "available" }, { day: "24", status: "available" },
           { day: "25", status: "available" }, { day: "26", status: "available" }, { day: "27", status: "available" },
