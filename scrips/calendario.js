d99bd88329e3db4dd02c0492486438833771614b
Dante <dantrux22@gmail.com>
2024-12-18 02:04:48 -0300

    .

--- scrips/calendario.js
+++ scrips/calendario.js
@@ -10,7 +10,7 @@ const months = [
           { day: "10", status: "available" }, { day: "11", status: "available" }, { day: "12", status: "available" },
           { day: "13", status: "reserved" }, { day: "14", status: "reserved" }, { day: "15", status: "reserved" },
           { day: "16", status: "reserved" }, { day: "17", status: "reserved" }, { day: "18", status: "reserved" },
-          { day: "19", status: "reserved" }, { day: "20", status: "available" }, { day: "21", status: "reserved" },
+          { day: "19", status: "available" }, { day: "20", status: "available" }, { day: "21", status: "reserved" },
           { day: "22", status: "reserved" }, { day: "23", status: "reserved" }, { day: "24", status: "reserved" },
           { day: "25", status: "reserved" }, { day: "26", status: "available" }, { day: "27", status: "available" },
           { day: "28", status: "reserved" }, { day: "29", status: "reserved" }, { day: "30", status: "available" },
@@ -23,7 +23,7 @@ const months = [
           { day: "10", status: "available" }, { day: "11", status: "available" }, { day: "12", status: "available" },
           { day: "13", status: "reserved" }, { day: "14", status: "reserved" }, { day: "15", status: "reserved" },
           { day: "16", status: "available" }, { day: "17", status: "available" }, { day: "18", status: "available" },
-          { day: "19", status: "available" }, { day: "20", status: "available" }, { day: "21", status: "reserved" },
+          { day: "19", status: "reserved" }, { day: "20", status: "available" }, { day: "21", status: "reserved" },
           { day: "22", status: "available" }, { day: "23", status: "available" }, { day: "24", status: "reserved" },
           { day: "25", status: "available" }, { day: "26", status: "available" }, { day: "27", status: "available" },
           { day: "28", status: "available" }, { day: "29", status: "available" }, { day: "30", status: "available" },
@@ -36,7 +36,7 @@ const months = [
       shifts: {
         day: [
           { day: "1", status: "reserved" }, { day: "2", status: "available" }, { day: "3", status: "available" },
-          { day: "4", status: "available" }, { day: "5", status: "available" }, { day: "6", status: "available" },
+          { day: "4", status: "available" }, { day: "5", status: "reserved" }, { day: "6", status: "available" },
           { day: "7", status: "available" }, { day: "8", status: "available" }, { day: "9", status: "available" },
           { day: "10", status: "available" }, { day: "11", status: "reserved" }, { day: "12", status: "available" },
           { day: "13", status: "available" }, { day: "14", status: "available" }, { day: "15", status: "available" },
