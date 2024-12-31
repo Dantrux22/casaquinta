b281b0608d1fbfd4cb633687ed861b817e6cd5bd
Dante <dantrux22@gmail.com>
2024-12-30 23:52:26 -0300

    .

--- scrips/calendario.js
+++ scrips/calendario.js
@@ -1,43 +1,12 @@
 // CALENDARIO
 const months = [
     {
-      name: "Diciembre 2024",
-      shifts: {
-        day: [
-          { day: "1", status: "available" }, { day: "2", status: "available" }, { day: "3", status: "available" },
-          { day: "4", status: "available" }, { day: "5", status: "available" }, { day: "6", status: "available" },
-          { day: "7", status: "reserved" }, { day: "8", status: "reserved" }, { day: "9", status: "reserved" },
-          { day: "10", status: "available" }, { day: "11", status: "available" }, { day: "12", status: "available" },
-          { day: "13", status: "reserved" }, { day: "14", status: "reserved" }, { day: "15", status: "reserved" },
-          { day: "16", status: "reserved" }, { day: "17", status: "reserved" }, { day: "18", status: "reserved" },
-          { day: "19", status: "available" }, { day: "20", status: "available" }, { day: "21", status: "reserved" },
-          { day: "22", status: "reserved" }, { day: "23", status: "reserved" }, { day: "24", status: "reserved" },
-          { day: "25", status: "reserved" }, { day: "26", status: "available" }, { day: "27", status: "available" },
-          { day: "28", status: "reserved" }, { day: "29", status: "reserved" }, { day: "30", status: "available" },
-          { day: "31", status: "reserved" }
-        ],
-        night: [
-          { day: "1", status: "available" }, { day: "2", status: "available" }, { day: "3", status: "available" },
-          { day: "4", status: "available" }, { day: "5", status: "available" }, { day: "6", status: "available" },
-          { day: "7", status: "available" }, { day: "8", status: "reserved" }, { day: "9", status: "available" },
-          { day: "10", status: "available" }, { day: "11", status: "available" }, { day: "12", status: "available" },
-          { day: "13", status: "reserved" }, { day: "14", status: "reserved" }, { day: "15", status: "reserved" },
-          { day: "16", status: "available" }, { day: "17", status: "available" }, { day: "18", status: "available" },
-          { day: "19", status: "reserved" }, { day: "20", status: "available" }, { day: "21", status: "reserved" },
-          { day: "22", status: "available" }, { day: "23", status: "available" }, { day: "24", status: "reserved" },
-          { day: "25", status: "available" }, { day: "26", status: "available" }, { day: "27", status: "available" },
-          { day: "28", status: "available" }, { day: "29", status: "available" }, { day: "30", status: "available" },
-          { day: "31", status: "reserved" }
-        ]
-      }
-    },
-    {
       name: "Enero 2025",
       shifts: {
         day: [
           { day: "1", status: "reserved" }, { day: "2", status: "available" }, { day: "3", status: "available" },
           { day: "4", status: "available" }, { day: "5", status: "reserved" }, { day: "6", status: "available" },
-          { day: "7", status: "available" }, { day: "8", status: "available" }, { day: "9", status: "available" },
+          { day: "7", status: "available" }, { day: "8", status: "available" }, { day: "9", status: "reserved" },
           { day: "10", status: "available" }, { day: "11", status: "reserved" }, { day: "12", status: "reserved" },
           { day: "13", status: "available" }, { day: "14", status: "available" }, { day: "15", status: "available" },
           { day: "16", status: "available" }, { day: "17", status: "available" }, { day: "18", status: "reserved" },
@@ -73,7 +42,7 @@ const months = [
             { day: "13", status: "available" }, { day: "14", status: "available" }, { day: "15", status: "reserved" },
             { day: "16", status: "available" }, { day: "17", status: "available" }, { day: "18", status: "available" },
             { day: "19", status: "available" }, { day: "20", status: "available" }, { day: "21", status: "available" },
-            { day: "22", status: "available" }, { day: "23", status: "available" }, { day: "24", status: "available" },
+            { day: "22", status: "available" }, { day: "23", status: "available" }, { day: "24", status: "reserved" },
             { day: "25", status: "available" }, { day: "26", status: "available" }, { day: "27", status: "available" },
             { day: "28", status: "available" }
           ],
