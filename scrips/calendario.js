a9baf1fa595b9cae827dc58ab3c6a05c98664918
Dante <dantrux22@gmail.com>
2025-01-09 12:24:24 -0300

    .

--- scrips/calendario.js
+++ scrips/calendario.js
@@ -35,7 +35,7 @@ const months = [
          name: "Febrero 2025",
         shifts: {
           day: [
-            { day: "1", status: "reserved" }, { day: "2", status: "available" }, { day: "3", status: "available" },
+            { day: "1", status: "reserved" }, { day: "2", status: "reserved" }, { day: "3", status: "available" },
             { day: "4", status: "available" }, { day: "5", status: "available" }, { day: "6", status: "available" },
             { day: "7", status: "available" }, { day: "8", status: "reserved" }, { day: "9", status: "reserved" },
             { day: "10", status: "available" }, { day: "11", status: "available" }, { day: "12", status: "available" },
